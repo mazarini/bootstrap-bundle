c3aa84dde954b9030c9382da5fb000a004235c77
Mazarini <mazarini@protonmail.com>
2019-12-01 14:20:17 +0100

    Adapt to tools and test bundles

--- src/Controller/StepController.php
+++ src/Controller/StepController.php
@@ -9,7 +9,7 @@
  * the Free Software Foundation, either version 3 of the License, or (at your
  * option) any later version.
  *
- * mazarini/pagination-bundle is distributed in the hope that it will be useful,
+ * mazarini/bootstrap-bundle is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
  * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
  * more details.
@@ -21,6 +21,7 @@ namespace App\Controller;
 
 use Mazarini\TestBundle\Controller\StepController as BaseController;
 use Mazarini\TestBundle\Fake\Pagination;
+use Mazarini\ToolsBundle\Controller\ControllerAbstract;
 use Mazarini\ToolsBundle\Data\Data;
 use Mazarini\ToolsBundle\Href\Hrefs;
 use Symfony\Component\HttpFoundation\RequestStack;
@@ -37,16 +38,23 @@ class StepController extends BaseController
         $this->parameters['datas'] = $this->getDatas($requestStack);
     }
 
+    protected function InitUrl(Data $data): ControllerAbstract
+    {
+        $data->gethrefs()->setCurrentUrl('#page_'.$data->getPagination()->getCurrentPage());
+
+        return $this;
+    }
+
     private function getDatas(RequestStack $requestStack): array
     {
         $datas = [];
-        $a = [0, 1, 5, 6, 9];
+        $a = [0, 1, 45, 56, 89];
         foreach ($a as $i) {
             $datas[] = $this->getData($requestStack, new Pagination(3, $i));
         }
         $a = [1, 2, 5, 6];
         foreach ($a as $i) {
-            $datas[] = $this->getData($requestStack, new Pagination($i, 6));
+            $datas[] = $this->getData($requestStack, new Pagination($i, 60));
         }
 
         return $datas;
@@ -58,6 +66,7 @@ class StepController extends BaseController
         $data->setPagination($pagination);
         $data->setHrefs(new Hrefs($requestStack));
         $this->InitPaginationUrl($data);
+        $data->gethrefs()->setCurrentUrl('#page_'.$pagination->getCurrentPage());
 
         return $data;
     }
