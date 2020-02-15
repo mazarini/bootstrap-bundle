26d6546228d24851e71d7c08ef7678373c65a494
mazarini <mazarini@protonmail.com>
2020-02-15 15:30:54 +0100

    Clean / test-bundle

--- src/Controller/StepController.php
+++ src/Controller/StepController.php
@@ -20,10 +20,7 @@
 namespace App\Controller;
 
 use Mazarini\TestBundle\Controller\StepController as BaseController;
-use Mazarini\TestBundle\Fake\UrlGenerator;
 use Mazarini\ToolsBundle\Data\Data;
-use Mazarini\ToolsBundle\Data\Link;
-use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -31,35 +28,10 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class StepController extends BaseController
 {
-    /**
-     * @var UrlGenerator
-     */
-    protected $router;
-
     protected function afterAction(string $action): void
     {
+        parent::afterAction($action);
         $this->parameters['datas'] = $this->getDatas();
-        $this->parameters['symfony']['version'] = Kernel::VERSION;
-        $this->parameters['php']['version'] = PHP_VERSION;
-        $this->parameters['php']['extensions'] = get_loaded_extensions();
-        asort($this->parameters['php']['extensions'], SORT_FLAG_CASE | SORT_NATURAL);
-
-        $this->parameters['dataCrud'] = $this->fakeFactory->getCrudData();
-        $this->parameters['dataPagination'] = $this->fakeFactory->getPaginationData();
-        $this->parameters['dataLinks'] = $this->fakeFactory->getLinksData();
-
-        $this->parameters['tree'] = $tree = $this->fakeFactory->getTree('Tree', 'item', 5);
-        $tree['item-1'] = $item1 = $this->fakeFactory->getTree('Item-1', 'item-1', 2);
-        $item1['item-1-1'] = $this->fakeFactory->getTree('Item-1-1', 'item-1-1', 3);
-        $item1['item-1-2'] = $this->fakeFactory->getTree('Item-1-2', 'item-1-2', 2);
-        $tree['item-2'] = $this->fakeFactory->getTree('Item-2', 'item-2', 2);
-        $tree['item-4'] = $this->fakeFactory->getTree('Item-4', 'item-4', 2);
-
-        $this->parameters['list'] = $this->fakeFactory->getLinks('item', 7);
-        $this->parameters['list']['item-2'] = new Link('item-2', '#', 'Disable');
-        $this->parameters['list']['item-6'] = new Link('item-6', '', 'Active');
-
-        $this->parameters['dataLinks'] = $this->fakeFactory->getLinksData();
     }
 
     /**
